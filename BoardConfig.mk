6eca91ef0fb9dd52a3332b8d6f4f2b1177086c86
Wim N <wimaen@gmail.com>
2018-07-31 22:40:11 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@
 #
 
 # inherit from Oppo msm8974-common
--include device/oppo/msm8974-common/BoardConfigCommon.mk
+-include device/oppo/common/BoardConfigCommon.mk
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
